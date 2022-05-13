e2403f9bd226e3a1f67daa49469a2a93467fd385
Alaa Rihan <alaa.addeen@gmail.com>
2022-05-14 01:34:31 +0300

    Show graphics view mode in the project edit mode

--- src/components/AppEditor.vue
+++ src/components/AppEditor.vue
@@ -104,8 +104,15 @@ export default {
       }
     });
 
+    const graphicTab = ref("data");
+    const graphicSlide = ref(0);
+    const graphicFullscreen = ref(false);
+
     return {
       appTab,
+      graphicTab,
+      graphicSlide,
+      graphicFullscreen,
       imageServerUrl: process.env.API_URL + "file/",
       initialPagination: {
         sortBy: "desc",
@@ -125,128 +132,107 @@ export default {
     <app-tabs v-model="appTab" />
     <q-tab-panels v-model="appTab" animated keep-alive class="shadow-2 rounded-borders">
       <q-tab-panel name="inputs">
-        <grid-editor
-          v-if="appData?.inputs"
-          :rows="appData.inputs"
-          :newRow="typesNewRow.inputs"
-          :columns="store.gridColumns.inputs"
-          @cell-changed="
+        <grid-editor v-if="appData?.inputs" :rows="appData.inputs" :newRow="typesNewRow.inputs"
+          :columns="store.gridColumns.inputs" @cell-changed="
             $emit('cellChanged', { event: $event, field: 'inputs' })
-          "
-          @rows-removed="
-            $emit('rowsRemoved', { event: $event, field: 'inputs' })
-          "
-          @row-added="$emit('rowAdded', { event: $event, field: 'inputs' })"
-        />
+          " @rows-removed="
+  $emit('rowsRemoved', { event: $event, field: 'inputs' })
+" @row-added="$emit('rowAdded', { event: $event, field: 'inputs' })" />
       </q-tab-panel>
 
       <q-tab-panel name="outputs">
-        <grid-editor
-          v-if="appData?.outputs"
-          :rows="appData.outputs"
-          :newRow="typesNewRow.outputs"
-          :columns="store.gridColumns.outputs"
-          @cell-changed="
+        <grid-editor v-if="appData?.outputs" :rows="appData.outputs" :newRow="typesNewRow.outputs"
+          :columns="store.gridColumns.outputs" @cell-changed="
             $emit('cellChanged', { event: $event, field: 'outputs' })
-          "
-          @rows-removed="
-            $emit('rowsRemoved', { event: $event, field: 'outputs' })
-          "
-          @row-added="$emit('rowAdded', { event: $event, field: 'outputs' })"
-        />
+          " @rows-removed="
+  $emit('rowsRemoved', { event: $event, field: 'outputs' })
+" @row-added="$emit('rowAdded', { event: $event, field: 'outputs' })" />
       </q-tab-panel>
 
       <q-tab-panel name="variables">
-        <grid-editor
-          v-if="appData?.variables"
-          :rows="appData.variables"
-          :newRow="typesNewRow.variables"
-          :columns="store.gridColumns.variables"
-          @cell-changed="
+        <grid-editor v-if="appData?.variables" :rows="appData.variables" :newRow="typesNewRow.variables"
+          :columns="store.gridColumns.variables" @cell-changed="
             $emit('cellChanged', { event: $event, field: 'variables' })
-          "
-          @rows-removed="
-            $emit('rowsRemoved', { event: $event, field: 'variables' })
-          "
-          @row-added="$emit('rowAdded', { event: $event, field: 'variables' })"
-        />
+          " @rows-removed="
+  $emit('rowsRemoved', { event: $event, field: 'variables' })
+" @row-added="$emit('rowAdded', { event: $event, field: 'variables' })" />
       </q-tab-panel>
       <q-tab-panel name="programs">
-        <grid-editor
-          v-if="appData?.programs"
-          :rows="appData.programs"
-          :newRow="typesNewRow.programs"
-          :columns="store.gridColumns.programs"
-          @cell-changed="
+        <grid-editor v-if="appData?.programs" :rows="appData.programs" :newRow="typesNewRow.programs"
+          :columns="store.gridColumns.programs" @cell-changed="
             $emit('cellChanged', { event: $event, field: 'programs' })
-          "
-          @rows-removed="
-            $emit('rowsRemoved', { event: $event, field: 'programs' })
-          "
-          @row-added="$emit('rowAdded', { event: $event, field: 'programs' })"
-        />
+          " @rows-removed="
+  $emit('rowsRemoved', { event: $event, field: 'programs' })
+" @row-added="$emit('rowAdded', { event: $event, field: 'programs' })" />
       </q-tab-panel>
       <q-tab-panel name="pids">
-        <grid-editor
-          v-if="appData?.pids"
-          :rows="appData.pids"
-          :newRow="typesNewRow.pids"
-          :columns="store.gridColumns.pids"
-          @cell-changed="$emit('cellChanged', { event: $event, field: 'pids' })"
+        <grid-editor v-if="appData?.pids" :rows="appData.pids" :newRow="typesNewRow.pids"
+          :columns="store.gridColumns.pids" @cell-changed="$emit('cellChanged', { event: $event, field: 'pids' })"
           @rows-removed="$emit('rowsRemoved', { event: $event, field: 'pids' })"
-          @row-added="$emit('rowAdded', { event: $event, field: 'pids' })"
-        />
+          @row-added="$emit('rowAdded', { event: $event, field: 'pids' })" />
       </q-tab-panel>
       <q-tab-panel name="graphics">
-        <grid-editor
-          v-if="appData?.graphics"
-          :rows="appData.graphics"
-          :newRow="typesNewRow.graphics"
-          :columns="store.gridColumns.graphics"
-          :row-height="80"
-          :type="type"
-          :slug="slug"
-          @cell-changed="
-            $emit('cellChanged', { event: $event, field: 'graphics' })
-          "
-          @rows-removed="
-            $emit('rowsRemoved', { event: $event, field: 'graphics' })
-          "
-          @row-added="$emit('rowAdded', { event: $event, field: 'graphics' })"
-        />
+        <q-tabs v-model="graphicTab" class="bg-cyan-8 text-white shadow-2" align="justify" indicator-color="pink-13"
+          inline-label dense>
+          <q-tab name="data" icon="table_rows" label="Data" />
+          <q-tab name="pictures" icon="photo_library" label="Pictures" />
+        </q-tabs>
+        <q-tab-panels v-model="graphicTab" animated class="shadow-2 rounded-borders">
+          <q-tab-panel name="data">
+            <grid-editor v-if="appData?.graphics" :rows="appData.graphics" :newRow="typesNewRow.graphics"
+              :columns="store.gridColumns.graphics" :row-height="80" :type="type" :slug="slug" @cell-changed="
+                $emit('cellChanged', { event: $event, field: 'graphics' })
+              " @rows-removed="
+  $emit('rowsRemoved', { event: $event, field: 'graphics' })
+" @row-added="
+  $emit('rowAdded', { event: $event, field: 'graphics' })
+" />
+          </q-tab-panel>
+
+          <q-tab-panel name="pictures">
+            <q-carousel swipeable animated arrows keep-alive control-type="regular" control-color="white"
+              control-text-color="black" v-model="graphicSlide" v-model:fullscreen="graphicFullscreen"
+              class="bg-black text-white rounded-borders">
+              <q-carousel-slide v-for="(graphicRow, index) in appData.graphics" :key="graphicRow.id" :name="index"
+                :img-src="
+                  graphicRow.picture?.path
+                    ? imageServerUrl + graphicRow.picture.path + '?w=1200'
+                    : '../assets/placeholder.png'
+                ">
+                <div class="absolute left-0 bottom-0 custom-caption bg-black/60 px-4 py-2 right-auto">
+                  <div class="text-white">{{ graphicRow.picture?.name }}</div>
+                </div>
+              </q-carousel-slide>
+              <template v-slot:control>
+                <q-carousel-control position="bottom-right" :offset="[18, 18]">
+                  <q-btn push round dense color="white" text-color="black"
+                    :icon="graphicFullscreen ? 'fullscreen_exit' : 'fullscreen'"
+                    @click="graphicFullscreen = !graphicFullscreen" />
+                </q-carousel-control>
+              </template>
+            </q-carousel>
+          </q-tab-panel>
+        </q-tab-panels>
       </q-tab-panel>
       <q-tab-panel name="holidays">
-        <grid-editor
-          v-if="appData?.holidays"
-          :rows="appData.holidays"
-          :newRow="typesNewRow.holidays"
-          :columns="store.gridColumns.holidays"
-          @cell-changed="
+        <grid-editor v-if="appData?.holidays" :rows="appData.holidays" :newRow="typesNewRow.holidays"
+          :columns="store.gridColumns.holidays" @cell-changed="
             $emit('cellChanged', { event: $event, field: 'holidays' })
-          "
-          @rows-removed="
-            $emit('rowsRemoved', { event: $event, field: 'holidays' })
-          "
-          @row-added="$emit('rowAdded', { event: $event, field: 'holidays' })"
-        />
+          " @rows-removed="
+  $emit('rowsRemoved', { event: $event, field: 'holidays' })
+" @row-added="$emit('rowAdded', { event: $event, field: 'holidays' })" />
       </q-tab-panel>
       <q-tab-panel name="schedules">
-        <grid-editor
-          v-if="appData?.schedules"
-          :rows="appData.schedules"
-          :newRow="typesNewRow.schedules"
-          :columns="store.gridColumns.schedules"
-          @cell-changed="
+        <grid-editor v-if="appData?.schedules" :rows="appData.schedules" :newRow="typesNewRow.schedules"
+          :columns="store.gridColumns.schedules" @cell-changed="
             $emit('cellChanged', { event: $event, field: 'schedules' })
-          "
-          @rows-removed="
-            $emit('rowsRemoved', { event: $event, field: 'schedules' })
-          "
-          @row-added="$emit('rowAdded', { event: $event, field: 'schedules' })"
-        />
+          " @rows-removed="
+  $emit('rowsRemoved', { event: $event, field: 'schedules' })
+" @row-added="$emit('rowAdded', { event: $event, field: 'schedules' })" />
       </q-tab-panel>
     </q-tab-panels>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
